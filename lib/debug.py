1167841a11c4b769ed945e52fa1d0d86d310bda4
Abiud Kiprotich <abiud.kiprotich@student.moringaschool.com>
2024-09-11 10:52:47 +0300

    completed the lab

--- lib/debug.py
+++ lib/debug.py
@@ -6,4 +6,22 @@ from department import Department
 
 import ipdb
 
+
+payroll = Department.create("Payroll", "Building A, 5th Floor")
+print(payroll)
+
+
+hr = Department.create("Human Resources", "Building C, East Wing")
+print(hr)
+
+hr.name = 'HR'
+hr.location = "Building F, 10th Floor"
+hr.update()
+print(hr)
+
+hr.save()
+print(hr)
+print("Delete Payroll")
+payroll.delete()  # delete from db table, object still exists in memory
+print(payroll)
 ipdb.set_trace()
